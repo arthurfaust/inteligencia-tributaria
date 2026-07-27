499e0f36a1f859e0f7b35c385bc3d1b36111ec03
arthurfaust <arthur.f.locatelli@gmail.com>
2026-07-27 13:15:35 -0300

    fix(app): Configura o app.js

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
-// view engine setup
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { 
+    httpOnly: true,
+    secure: false 
+  }
+}));
+
+// Configuração do motor de templates (EJS)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rota principal da aplicação
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Tratamento de erros de rota não encontrada (404)
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Gerenciador global de erros
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
